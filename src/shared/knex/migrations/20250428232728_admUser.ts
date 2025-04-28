7f77f9938801aff183ca2cfd6371f61e9c82ff02
Elcio <elcio.cf@gmail.com>
2025-04-28 20:30:38 -0300

    add migrate to create usuarios_adm

---
+++ src/shared/knex/migrations/20250428232728_admUser.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("usuarios_adm", (t) => {
+        t.integer("cod_usuario_adm");
+        t.string("nome", 150);
+        t.string("login", 50);
+        t.string("senha", 255);
+        t.string("email", 200);
+        t.primary(["cod_usuario_adm"]);
+        t.timestamps(true, true);
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists("usuarios_adm");
+}
