cf0bfe2e2b8ea38f389231f7c71850aaa2e4ead7
Elcio <elcio.cf@gmail.com>
2025-05-12 20:54:28 -0300

    fixed CLI usecase getbypk

---
+++ src/modules/users/useCases/users/getByPKUser/GetByPKUserController.ts
@@ -0,0 +1,18 @@
+import { Response, Request } from "express";
+import { container } from "tsyringe";
+
+import { GetByPKUserUseCase } from "./GetByPKUserUseCase";
+
+class GetByPKUserController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { coduser } = request.params;
+        const cod_usuario = Number(coduser);
+
+        const getByPKUserUseCase = container.resolve(GetByPKUserUseCase);
+
+        const user = await getByPKUserUseCase.execute({ cod_usuario });
+        return response.status(200).json(user);
+    }
+}
+
+export { GetByPKUserController };
---
+++ src/modules/users/useCases/users/getByPKUser/GetByPKUserUseCase.ts
@@ -0,0 +1,29 @@
+import { User } from "@modules/users/entities/User";
+import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+    cod_usuario?: number;
+}
+
+@injectable()
+class GetByPKUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ cod_usuario }: IRequest): Promise<User> {
+        // check if has pk_field
+        if (!cod_usuario) {
+            throw new AppError("O código PK da User deve ser informado.");
+        }
+
+        const User = await this.usersRepository.getByPK(cod_usuario);
+        return User;
+    }
+}
+
+export { GetByPKUserUseCase };
