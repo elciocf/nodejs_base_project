52ec616d19a040c289f3952aa3a0c302cb9e9df7
Elcio <elcio.cf@gmail.com>
2025-05-12 21:15:34 -0300

    Update auth.ts

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,10 +1,10 @@
 export default {
     secret_token:
-        "$2y$06$rnpAPsIPbhivf8ELlCaU1u96Kn2Pug7rYgMoyX77Mhe/bwkLj33wO",
+        "SEU_HASH",
     expires_in_token: "20m",
     expires_in_token_minutes: 20,
     secret_refresh_token:
-        "$2y$06$rxKWdUhx7U8UJIjQjcnpF.IbCBvOIMpEYchMQoBxupNuCd/KCNEBm",
+        "SEU_REFRESH_TOKEN_HASH",
     expires_in_refresh_token: "7d",
     expires_refresh_token_days: 7,
 };
