1ebca40c79d2c6147187365fb4e87f8fc1cbb12b
Elcio <elcio.cf@gmail.com>
2025-05-12 21:14:31 -0300

    auth

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,8 +1,10 @@
 export default {
-    secret_token: "ADD_SEU_HASH_AQUI",
+    secret_token:
+        "$2y$06$rnpAPsIPbhivf8ELlCaU1u96Kn2Pug7rYgMoyX77Mhe/bwkLj33wO",
     expires_in_token: "20m",
     expires_in_token_minutes: 20,
-    secret_refresh_token: "ADD_SEU_REFRESH_HASH_AQUI",
+    secret_refresh_token:
+        "$2y$06$rxKWdUhx7U8UJIjQjcnpF.IbCBvOIMpEYchMQoBxupNuCd/KCNEBm",
     expires_in_refresh_token: "7d",
     expires_refresh_token_days: 7,
 };
